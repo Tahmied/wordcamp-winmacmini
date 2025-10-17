0d6fed9435a8d81bf787b5a81c73a9d0d5dd0dd3
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-10-17 18:45:14 +0600

    - removed cors system

--- src/app.js
+++ src/app.js
@@ -5,10 +5,7 @@ import { errorHandler } from "./Middlewares/errorHandler.middleware.js";
 
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(cors())
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
